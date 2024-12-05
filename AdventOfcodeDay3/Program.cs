b0a86568698e773a8e8f148ebdf9e550e1aab213
u231061 <u231061@YS-QUAD-02>
2024-12-05 13:26:37 +0000

    Add project files.

---
+++ AdventOfcodeDay3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AdventOfcodeDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            string[] split = input.Split(new string[] { "mul(" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
