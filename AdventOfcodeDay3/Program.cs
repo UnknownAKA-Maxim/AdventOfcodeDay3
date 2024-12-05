3020beceaf991be973dcfeab21ac59aa4b4d67b9
Unknown-AKAMaxim <maximfarnworth@outlook.com>
2024-12-05 15:08:53 +0000

    j

--- AdventOfcodeDay3/Program.cs
+++ AdventOfcodeDay3/Program.cs
@@ -12,6 +12,22 @@ namespace AdventOfcodeDay3
         {
             string input = File.ReadAllText("input.txt");
             string[] split = input.Split(new string[] { "mul(" }, StringSplitOptions.RemoveEmptyEntries);
+            int answer = 0;
+
+            foreach (string line in split)
+            {
+                string[] numbers = line.Split(',');
+                if (int.TryParse(numbers[0], out int value1))
+                {
+                    if (int.TryParse(numbers[1].Split(')')[0], out int value2))
+                    {
+                        answer += value1 * value2;
+                    }
+                }
+            }
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
         }
     }
 }
